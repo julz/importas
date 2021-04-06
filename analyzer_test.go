74fb575d5a0e9b549cd98ee40f80a6efb1cee770
Takumasa Sakao <tsakao@zlab.co.jp>
2021-04-06 13:51:40 +0900

    Add new test case

--- analyzer_test.go
+++ analyzer_test.go
@@ -24,6 +24,7 @@ func TestAnalyzer(t *testing.T) {
 			aliases: stringMap{
 				"fmt": "fff",
 				"os":  "stdos",
+				"io":  "iio",
 			},
 		},
 		{
--- testdata/src/a/a.go
+++ testdata/src/a/a.go
@@ -2,10 +2,12 @@ package a
 
 import (
 	wrong_alias "fmt"      // want `import "fmt" imported as "wrong_alias" but must be "fff" according to config`
+	"io"                   // want `import "io" imported without alias but must be with alias "iio" according to config`
 	wrong_alias_again "os" // want `import "os" imported as "wrong_alias_again" but must be "stdos" according to config`
 )
 
 func ImportAsWrongAlias() {
 	wrong_alias.Println("foo")
 	wrong_alias_again.Stdout.WriteString("bar")
+	io.Pipe()
 }
